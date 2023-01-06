4c70f6cc9cf99b67c2d609d38d36957a37d91277
Ronak000 <ronakshahlj@gmail.com>
2023-01-06 15:43:54 +0530

    End of Section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,12 +1,34 @@
+using API.Extensions;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }        
         public string UserName { get; set; }
-
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; } 
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new();
+
+        // public int GetAge()
+        // {
+        //     var today = (DateTime.UtcNow); // 05/01/2022 2:12
+
+        //     var age = today.Year - this.DateOfBirth.Year; // 2022  -  2000== 22
+
+        //     if(this.DateOfBirth.Date > today.AddYears(-age)) age--; // 04/01/2022 >  05/01/2000
+
+        //     return age;
+        // }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,7 @@
-using System.Text;
 using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,9 +14,25 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+using var scope = app.Services.CreateScope();
+var service = scope.ServiceProvider;
+
+try
+{
+    var context = service.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.Seedusers(context);
+}
+catch(Exception ex)
+{
+    var logger = service.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An Error occured During Migration");
+}
 
 app.Run();
